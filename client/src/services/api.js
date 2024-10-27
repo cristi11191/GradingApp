e4f09407e9e9113ab5a1e6cec1d12ecc0b52ca5b
Cristinel <druta.cristin@gmail.com>
2024-10-27 18:13:20 +0200

    Initial commit

---
+++ client/src/services/api.js
@@ -0,0 +1,14 @@
+// src/api.js
+const API_BASE_URL = 'http://localhost:5000';
+
+export const fetchHelloMessage = async () => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/api/hello`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch');
+    }
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
