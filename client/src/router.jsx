10a4001c08db4c660255e492d0d8ec2f3eb6240e
gladisBraete <braetegladis22@stud.ase.ro>
2024-11-11 18:50:07 +0200

    signup commit fail

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { createBrowserRouter, Navigate } from "react-router-dom";
 import DefaultLayout from './layouts/DefaultLayout.jsx';
 import Login from './components/Login/Login.jsx';
+import Signup from './components/Signup/Signup.jsx';
 import ProtectedRoutes from "./hooks/ProtectedRoutes.jsx";
 import MainContent from "./views/MainContent.jsx";
 import GuestLayout from "./layouts/GuestLayout.jsx";
@@ -31,6 +32,10 @@ const router = createBrowserRouter([
             {
                 path: '/login',
                 element: <Login />
+            },
+            {
+                path:"/signup",
+                element:<Signup/>
             }
         ]
     },
