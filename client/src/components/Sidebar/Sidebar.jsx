f18b23e54a44af17f1d8f45cd42c74c7d5ea89cc
gladisBraete <braetegladis22@stud.ase.ro>
2024-11-12 21:50:07 +0200

    nu mai pot

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Layout, Menu } from "antd";
 import { useNavigate } from "react-router-dom";
-import HomeIcon from "@mui/icons-material/Home";
+import MenuIcon from '@mui/icons-material/Menu';
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import AssignmentIcon from "@mui/icons-material/Assignment";
 import FolderIcon from "@mui/icons-material/Folder";
@@ -15,26 +15,40 @@ const Sidebar = () => {
   const [collapsed, setCollapsed] = useState(false);
   const navigate = useNavigate();
 
+  useEffect(() => {
+    const layoutElement = document.getElementById("layout");
+    if (layoutElement) {
+      if (collapsed) {
+        layoutElement.classList.add("closed");
+      } else {
+        layoutElement.classList.remove("closed");
+      }
+    }
+  }, [collapsed]);
+
   const handleLogout = () => {
     localStorage.removeItem("token");
     navigate("/login");
   };
 
-  const HandleProjects = () =>{
-    navigate("/projects");
-  }
-  const HandleDashboard = () =>{
-    navigate("/dashboard");
-  }
+  const handleToggleCollapse = () => {
+    setCollapsed(!collapsed);
+  };
 
-  // const toggleCollapse = () => {
-  //   setCollapsed(!collapsed);
-  // };
+  const HandleProjects = () => {
+    navigate("/projects");
+  };
+
+  const HandleDashboard = () => {
+    navigate("/dashboard");
+  };
 
   return (
-    <Layout>
-      <Sider collapsible collapsed={collapsed} onCollapse={setCollapsed} className="sidebar">
-        <HomeIcon className="homeIcon" />
+    <Layout id="layout">
+      <Sider collapsed={collapsed} className="sidebar">
+        <div className="menu-icon-container" onClick={handleToggleCollapse}>
+          <MenuIcon className="menu-icon" />
+        </div>
         <div className="menu-container">
           <Menu theme="dark" mode="inline">
             <Menu.Item key="dashboard" icon={<DashboardIcon />} onClick={HandleDashboard}>
@@ -53,16 +67,13 @@ const Sidebar = () => {
             <AccountBoxIcon />
             {!collapsed && <span>User</span>}
             <Menu theme="dark" mode="inline" className="menu-logout">
-              <Menu.Item key="logout" icon={<LogoutIcon />}  onClick={handleLogout}>
+              <Menu.Item key="logout" icon={<LogoutIcon />} onClick={handleLogout}>
                 Log Out
               </Menu.Item>
             </Menu>
           </div>
         </div>
       </Sider>
-      {/*<button onClick={toggleCollapse} className="toggle-button">*/}
-      {/*  {collapsed ? "Expand" : "Collapse"}*/}
-      {/*</button>*/}
     </Layout>
   );
 };
