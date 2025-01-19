d866d1fe321a3433c9c44f472d843a0efcabc9e8
Cristinel <druta.cristin@gmail.com>
2025-01-19 19:51:32 +0200

    seeds

--- server/prisma/seed.js
+++ server/prisma/seed.js
@@ -1,5 +1,6 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
+const bcrypt = require('bcrypt');
 
 async function main() {
     const hashedPassword = await bcrypt.hash('secret', 10);
